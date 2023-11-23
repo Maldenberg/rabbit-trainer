098d12b8cb1957ada2800436f4a94a84d858d00c
Sergey Maldenberg <maldenberg@mail.ru>
2023-11-23 08:37:54 +0300

    3 version

--- public/js/script.js
+++ public/js/script.js
@@ -28,7 +28,6 @@ function sendMessage() {
     });
 }
 
-// Функция для получения сообщений из очереди и их отображения
 function refreshMessages() {
   const queueMapping = {
     inbox: "inbox_queue",
@@ -50,14 +49,17 @@ function refreshMessages() {
         const allMessages = storedMessages.concat(newMessages);
 
         if (allMessages.length > 0) {
-          // Отображаем каждое сообщение
           allMessages.forEach((message) => {
             const messageElement = document.createElement("div");
-            messageElement.textContent = message;
+            const iconElement = document.createElement("i");
+            iconElement.classList.add("message-icon");
+            iconElement.innerHTML = "&#9993;"; // Иконка письма
+
+            messageElement.appendChild(iconElement);
+            messageElement.appendChild(document.createTextNode(message));
             messagesList.appendChild(messageElement);
           });
 
-          // Обновляем LocalStorage с новым списком сообщений
           localStorage.setItem(queueName, JSON.stringify(allMessages));
         } else {
           messagesList.textContent = "Нет сообщений...";
@@ -102,79 +104,3 @@ document.addEventListener("DOMContentLoaded", () => {
     clearStorageButton.addEventListener("click", clearMessages); // Добавляем обработчик для очистки
   }
 });
-
-/* // Функция для отправки сообщений
-function sendMessage() {
-  var messageText = document.getElementById("message").value;
-  fetch("/send", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ message: messageText }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      alert(data.status);
-      document.getElementById("message").value = ""; // Очистить текстовое поле
-    })
-    .catch((error) => {
-      console.error("Ошибка:", error);
-    });
-}
-
-// Функция для получения сообщений из очереди и их отображения
-function refreshMessages() {
-  const queueMapping = {
-    inbox: "inbox_queue",
-    important: "important_queue",
-    spam: "spam_queue",
-  };
-
-  Object.keys(queueMapping).forEach((section) => {
-    const queueName = queueMapping[section];
-    const storedMessages = JSON.parse(localStorage.getItem(queueName)) || [];
-
-    fetch(`/receive/${queueName}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const messagesList = document.getElementById(section);
-        messagesList.innerHTML = ""; // Очистить текущие сообщения
-
-        const newMessages = data.messages || [];
-        const allMessages = storedMessages.concat(newMessages);
-
-        if (allMessages.length > 0) {
-          // Отображаем каждое сообщение
-          allMessages.forEach((message) => {
-            const messageElement = document.createElement("div");
-            messageElement.textContent = message;
-            messagesList.appendChild(messageElement);
-          });
-
-          // Обновляем LocalStorage с новым списком сообщений
-          localStorage.setItem(queueName, JSON.stringify(allMessages));
-        } else {
-          messagesList.textContent = "Нет сообщений...";
-        }
-      })
-      .catch((error) => {
-        console.error("Ошибка при получении сообщений:", error);
-      });
-  });
-}
-
-// Добавление обработчика событий на кнопку отправки и обновления
-document.addEventListener("DOMContentLoaded", () => {
-  const sendButton = document.getElementById("send");
-  const refreshButton = document.getElementById("refresh");
-
-  if (sendButton) {
-    sendButton.addEventListener("click", sendMessage);
-  }
-
-  if (refreshButton) {
-    refreshButton.addEventListener("click", refreshMessages);
-  }
-});
- */
